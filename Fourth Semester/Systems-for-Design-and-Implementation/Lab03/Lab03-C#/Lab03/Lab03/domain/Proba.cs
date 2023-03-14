449d7ac0f0b368b1d03700cf6860ca132d6df990
Hatisi Bogdan <62288167+bogdanhatisi@users.noreply.github.com>
2023-03-15 01:46:52 +0200

    Lab03-C# fixes

--- Fourth Semester/Systems-for-Design-and-Implementation/Lab03/Lab03-C#/Lab03/Lab03/domain/Proba.cs
+++ Fourth Semester/Systems-for-Design-and-Implementation/Lab03/Lab03-C#/Lab03/Lab03/domain/Proba.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Lab03.domain
 {
-    internal class Class1
+    public class Proba : Identifiable<int>
     {
+
+        private String stil;
+
+        private int distanta;
+
+        public Proba(String stil, int distanta)
+        {
+            this.stil = this.stil;
+            this.distanta = this.distanta;
+        }
+
+        public String getStil()
+        {
+            return this.stil;
+        }
+
+        public int getDistanta()
+        {
+            return this.distanta;
+        }
+
+        public void setStil(String stil)
+        {
+            this.stil = this.stil;
+        }
+
+        public void setDistanta(int distanta)
+        {
+            this.distanta = this.distanta;
+        }
+
+       
+
+        public void setId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        int Identifiable<int>.getId()
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
