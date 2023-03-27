ba59d9cd9e5d488e8952d3fb71be235507fb9b4b
Hatisi Bogdan <62288167+bogdanhatisi@users.noreply.github.com>
2023-03-27 16:12:20 +0300

    Lab03 MPP

--- Fourth Semester/Systems-for-Design-and-Implementation/Lab03/Lab03-Java/src/domain/Participant.java
+++ Fourth Semester/Systems-for-Design-and-Implementation/Lab03/Lab03-Java/src/domain/Participant.java
@@ -3,12 +3,12 @@ package domain;
 public class Participant implements Identifiable<Integer>{
     private String nume;
     private Integer varsta;
-    private String probe;
+
 
     public Participant(String nume, Integer varsta, String probe) {
         this.nume = nume;
         this.varsta = varsta;
-        this.probe = probe;
+
     }
 
     public String getNume() {
@@ -19,9 +19,6 @@ public class Participant implements Identifiable<Integer>{
         return varsta;
     }
 
-    public String getProbe() {
-        return probe;
-    }
 
     public void setNume(String nume) {
         this.nume = nume;
---
+++ Fourth Semester/Systems-for-Design-and-Implementation/Lab03/Lab03-Java/src/repository/ParticipanDBRepository.java
@@ -0,0 +1,12 @@
+package repository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ParticipanDBRepository {
+
+}
