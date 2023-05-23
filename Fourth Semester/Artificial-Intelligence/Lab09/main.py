fee9b90c5bbef93c80866cdcb37a63bc155514c2
Hatisi Bogdan <62288167+bogdanhatisi@users.noreply.github.com>
2023-05-24 00:52:43 +0300

    Lab09-AI-Images

--- Fourth Semester/Artificial-Intelligence/Lab09/main.py
+++ Fourth Semester/Artificial-Intelligence/Lab09/main.py
@@ -1,47 +1,26 @@
 import sys
 import numpy as np
-import matplotlib
+import matplotlib as plt
+import nnfs
+from nnfs.datasets import spiral_data
+# np.random.seed(0)
 
-np.random.seed()
+nnfs.init()
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]
-     ]
-
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]
-           ]
-
-biases = [2, 3, 0.5]
-
-weights2 = [[0.1, -0.14, 0.5],
-            [-0.5, 0.12, -0.33],
-            [-0.44, 0.73, -0.13]
-            ]
-
-biases2 = [-1, 2, -0.5]
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-print(layer2_outputs)
-
-layer_outputs = []
-
-# for neuron_weights, neuron_bias in zip(weights,biases):
-#     neuron_output = 0
-#     for n_input, weight in zip(inputs,neuron_weights):
-#         neuron_output += n_input*weight
-#     neuron_output += neuron_bias
-#     layer_outputs.append(neuron_output)
-
-# print(layer_outputs)
+X, y = spiral_data(100, 3)
+print(X)
 
 
 class Layer_Dense:
-    def __init__(self):
-        pass
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons), dtype=float)
 
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
